c2b241b6bfec9b41457e35feec5543bb4129967c
Julien <julien@bebl.fr>
2026-02-06 15:54:17 +0100

    modif buzzer gpiozero

--- V2/test/test_buzzer.py
+++ V2/test/test_buzzer.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
-import pigpio
-import time
 
-GPIO_BUZZ = 26
-FREQ = 2000
-DUTY = 500000   # pigpio duty: 0..1_000_000 (50% => 500000)
+from gpiozero import PWMOutputDevice
+from time import sleep
 
-pi = pigpio.pi()
-if not pi.connected:
-    raise RuntimeError("pigpio daemon non connecté. Lance: sudo systemctl enable --now pigpiod")
+BUZZER_GPIO = 26
+FREQ = 2000      # Hz
+DUTY = 0.5       # 50 %
+
+buzzer = PWMOutputDevice(
+    pin=BUZZER_GPIO,
+    frequency=FREQ,
+    initial_value=0.0
+)
 
 try:
-    pi.set_mode(GPIO_BUZZ, pigpio.OUTPUT)
-    pi.set_PWM_frequency(GPIO_BUZZ, FREQ)
-    pi.set_PWM_range(GPIO_BUZZ, 1000000)
-    pi.set_PWM_dutycycle(GPIO_BUZZ, DUTY)
+    # Buzzer ON 2 s
+    buzzer.value = DUTY
+    sleep(2.0)
 
-    time.sleep(2.0)
-
-    pi.set_PWM_dutycycle(GPIO_BUZZ, 0)
-    time.sleep(0.5)
+    # OFF
+    buzzer.off()
+    sleep(0.5)
 
     # Bip-bip-bip
     for _ in range(3):
-        pi.set_PWM_dutycycle(GPIO_BUZZ, DUTY)
-        time.sleep(0.2)
-        pi.set_PWM_dutycycle(GPIO_BUZZ, 0)
-        time.sleep(0.2)
+        buzzer.value = DUTY
+        sleep(0.2)
+        buzzer.off()
+        sleep(0.2)
+
 finally:
-    pi.stop()
+    buzzer.close()
